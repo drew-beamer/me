34f94cc70f6c8f6b551d79c3134312a3cd30ebd5
drew-beamer <drbeamer@davidson.edu>
2023-02-23 11:55:12 -0500

    smoother animation

--- components/ui-components/pageWrapper.tsx
+++ components/ui-components/pageWrapper.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from "framer-motion";
-import { usePathname } from "next/navigation";
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
 
