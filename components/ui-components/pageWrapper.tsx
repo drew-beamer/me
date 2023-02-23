11ea9a6fc0216234ddda52f1441b0e3231c6c3f9
drew-beamer <drbeamer@davidson.edu>
2023-02-23 11:49:50 -0500

    testing if removing key does anything

--- components/ui-components/pageWrapper.tsx
+++ components/ui-components/pageWrapper.tsx
@@ -6,7 +6,6 @@ import { usePathname } from "next/navigation";
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
 
     return <motion.div
-        key={usePathname()}
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ type: "spring", stiffness: 175 }}
