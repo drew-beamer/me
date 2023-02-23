66df6309a25cf54ea0e79a8795621a0257ab3058
drew-beamer <drbeamer@davidson.edu>
2023-02-23 11:54:52 -0500

    smoother animation

--- components/ui-components/pageWrapper.tsx
+++ components/ui-components/pageWrapper.tsx
@@ -6,9 +6,9 @@ import { usePathname } from "next/navigation";
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
 
     return <motion.div
-        initial={{ opacity: 0, x: -100 }}
+        initial={{ opacity: 0, x: -250 }}
         animate={{ opacity: 1, x: 0 }}
-        transition={{ type: "spring", stiffness: 175 }}
+        transition={{ x: { type: "spring", stiffness: 50, delay: 0.1, duration: 0.75 } }}
     >
         {children}
     </motion.div>
