6ad9ead1c0957f3cc69e9f6da45d2511577cb95e
Drew Beamer <85971377+drew-beamer@users.noreply.github.com>
2023-02-23 01:01:18 -0500

    remove exit prop

--- components/ui-components/pageWrapper.tsx
+++ components/ui-components/pageWrapper.tsx
@@ -9,7 +9,6 @@ export default function PageWrapper({ children }: { children: React.ReactNode })
         key={usePathname()}
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: 100 }}
         transition={{ type: "spring", stiffness: 175 }}
     >
         {children}
