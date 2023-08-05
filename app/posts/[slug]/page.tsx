e9af2e253f878683362d49c04c9a9c530c3cf1e9
drew-beamer <drbeamer@davidson.edu>
2023-08-05 19:25:27 -0400

    adds blur test

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -41,9 +41,28 @@ export async function generateMetadata({ params }: { params: { slug: string } })
     }
 }
 
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>∏
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
+
 const NextImage = (props) => {
     return <div className="blog-post-image">
-        <Image src={props.src} placeholder="blur" blurDataURL={props.src} alt={props.alt} {...props} className="px-4 py-4 relative bottom-4" />
+        <Image src={props.src} placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`} alt={props.alt} {...props} className="px-4 py-4 relative bottom-4" />
     </div>
 }
 
