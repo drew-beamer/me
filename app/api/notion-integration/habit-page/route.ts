ed2b3f45fb2178990de536582d801a579c47900c
drew-beamer <drbeamer@davidson.edu>
2023-09-18 21:32:32 -0400

    bug fix for timezone

--- app/api/notion-integration/habit-page/route.ts
+++ app/api/notion-integration/habit-page/route.ts
@@ -9,20 +9,21 @@ import { Client } from "@notionhq/client";
  */
 export async function GET() {
     const notion = new Client({ auth: process.env.NOTION_TOKEN });
-    const currDate = new Date();
-    const month = ("0" + (currDate.getMonth() + 1)).slice(-2);
-    const day = ("0" + currDate.getDate()).slice(-2);
-    const currDateString = `${currDate.getFullYear()}-${month}-${day}`;
+    const currDate = new Date(new Date().toLocaleDateString("en-US", {
+        timeZone: "America/New_York",
+    })).toISOString();
+    console.log(currDate)
     if (process.env.NOTION_HABIT_DATABASE !== undefined) {
         const response = await notion.databases.query({
             database_id: process.env.NOTION_HABIT_DATABASE,
             filter: {
                 property: "Date",
                 date: {
-                    equals: currDateString,
+                    equals: currDate.slice(0, 10)
                 },
             },
         });
+        console.log(response)
         if ("properties" in response.results[0]) {
             const pageId = response.results[0].id;
             const pageProperties = response.results[0]?.properties;
