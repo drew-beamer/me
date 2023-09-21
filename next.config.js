f690afaabe0775bf7dfae50132dc687b65f026c6
drew-beamer <drbeamer@davidson.edu>
2023-09-21 10:26:35 -0400

    updates to 13.5.2

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-  }
-  
-  module.exports = withContentlayer(nextConfig);
+}
+
+module.exports = withContentlayer(nextConfig);
