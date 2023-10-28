ae76e2c75487f4e84f9060d84e9af2467e86ea6c
drew-beamer <drbeamer@davidson.edu>
2023-10-28 00:51:43 -0400

    remove background lines

--- app/page.tsx
+++ app/page.tsx
@@ -14,31 +14,9 @@ export const metadata = {
   },
 };
 
-function ZoomLine({ width, delay }: { width?: number; delay: number }) {
-  const w = width || 100;
-  return (
-    <div style={{ width: `${w}%` }}>
-      <div
-        style={{
-          animation: `${animations.fadeoutLR} 500ms ease, ${animations.pulse} 5s ease infinite`,
-          animationDelay: `${delay}ms`,
-          animationFillMode: "both",
-        }}
-        className={`mx-4 rounded-full my-4 border-dashed h-1 ${
-          Math.random() < 0.5 ? "bg-primary" : "bg-foreground"
-        }`}></div>
-    </div>
-  );
-}
-
 export default function Home() {
   return (
     <div className="w-full relative flex justify-center">
-      <div className="absolute z-[-1] w-full flex justify-center flex-wrap space-x-4 px-2 sm:px-8 max-h-[calc(100vh-4rem)] overflow-hidden">
-        {Array.from({ length: 50 }).map((_, i) => (
-          <ZoomLine key={i} width={Math.random() * 35 + 15} delay={i * 25} />
-        ))}
-      </div>
       <section className="w-full flex flex-wrap items-center justify-around text-center">
         <div className="flex flex-col justify-center items-center w-full">
           <h1 className="text-4xl md:text-5xl font-bold mt-32 leading-relaxed">
