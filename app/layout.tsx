54af65c7ff5350f32ab2393c0efd28e6e89e7dc6
Drew Beamer <andrewmbeamer@gmail.com>
2024-05-18 11:07:45 -0700

    build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,8 @@ import "@/styles/globals.css";
 import React from "react";
 import Navigation from "@/components/navigation";
 import { KEYWORDS } from "lib/utils/constants";
-import { Poppins, Source_Code_Pro as SourceCodePro } from "next/font/google";
 import cn from "@/lib/utils/cn";
-
-export const fontSans = Poppins({
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
-    variable: "--font-sans",
-});
-
-export const fontMono = SourceCodePro({
-    subsets: ["latin"],
-    weight: "400",
-    variable: "--font-mono",
-});
+import { fontSans, fontMono } from "./fonts";
 
 export const metadata = {
     creator: "Drew Beamer",
