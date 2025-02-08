8dbd978b9eddac8298e76b7b266b194259201c7e
Drew Beamer <andrewmbeamer@gmail.com>
2025-02-08 01:13:21 -0500

    resolve conflict

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -26,8 +26,8 @@ const Navbar: React.FC = () => {
   return (
     <nav
       className={cn(
-        "flex justify-between items-center bg-card/0 py-1 px-6 fixed top-0 left-0 right-0 z-50 rounded-b-xl transition-colors duration-300",
-        hasScrolled && "bg-card/60 backdrop-blur-md"
+        "flex justify-between items-center py-1 px-6 fixed top-0 left-0 right-0 z-50 rounded-b-xl transition-all duration-300",
+        hasScrolled ? "bg-card/60 backdrop-blur-md" : "bg-card/0"
       )}
     >
       <div className="text-lg font-mono">
