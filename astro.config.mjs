ac08dc990251c91eff689efd7e5fbc187a5c50ac
Drew Beamer <andrewmbeamer@gmail.com>
2025-10-04 13:37:09 -0400

    configure biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,23 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: "_blank",
-          rel: "noopener noreferrer",
-        },
-      ]
-    ],
-  },
+	adapter: vercel(),
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+			],
+		],
+	},
 });
--- src/schemas.ts
+++ src/schemas.ts
@@ -1,8 +1,8 @@
 import { z } from "astro:content";
 
 export const ProjectMetadataSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  image: z.string().optional(),
-  priority: z.number().default(0),
+	title: z.string(),
+	description: z.string(),
+	image: z.string().optional(),
+	priority: z.number().default(0),
 });
